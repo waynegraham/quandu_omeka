3e4f5c2936426047f3fd75f774134c0090f827d5
Wayne Graham <wayne.graham@virginia.edu>
2013-07-02 09:54:06 -0400

    basic rails app

---
+++ Procfile
@@ -0,0 +1,2 @@
+rails:    bundle exec rails server -p $PORT
+guard:    bundle exec guard
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuanduOmeka::Application.config.secret_token = '7209265320406fb6c9bb4c07adc068c085a05b59a8d8632c2825d66b8e4748711910e3b8161c2e451ef2923b060daaf3abb480a37a783c7ac39a88c14e3602ee'
