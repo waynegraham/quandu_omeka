4e0ac6f64372cc5fcdf344822a613ac9679cd700
Wayne Graham <wayne.graham@virginia.edu>
2013-07-05 11:35:50 -0400

    Refactor templated stuff in to helpers

--- app/helpers/instances_helper.rb
+++ app/helpers/instances_helper.rb
@@ -1,2 +1,45 @@
 module InstancesHelper
+
+  def apache_config(stage)
+    html = <<-HTTPDCONFIG
+      <pre>
+&lt;VirtualHost *:80>
+    DocumentRoot /usr/local/projects/#{ @instance.course.downcase }/current
+
+    ServerName #{stage}.#{ @instance.course.downcase }.#{ENV['domain'] || 'localhost' }
+    ServerAlias #{stage}.#{ @instance.course.downcase }.#{ENV['domain'] || 'localhost' }
+
+    ErrorLog logs/#{ @instance.course.downcase }.omeka-error_log
+    CustomLog logs/#{ @instance.course.downcase }.omeka-access_log common
+
+    &lt;Directory "/usr/local/projects/#{ @instance.course.downcase }/current">
+      Options FollowSymlinks
+      AllowOverride All
+      Order allow,deny
+      Allow from all
+    &lt;/Directory>
+
+
+&lt;/VirtualHost>
+      </pre>
+
+    HTTPDCONFIG
+    html
+  end
+
+  def db_config(stage)
+    html = <<-DBCONFIG
+      <pre>
+[database]
+host     = "#{ ENV['db_host'] ||= 'localhost' }"
+username = "#{ ENV['db_user'] ||= 'username' }"
+password = "#{ ENV['db_password'] ||= 'password' }"
+name     = "omeka_#{ stage }"
+prefix   = "#{ @instance.course.downcase }_"
+;port     = ""
+      </pre>
+    DBCONFIG
+    html
+  end
+
 end
